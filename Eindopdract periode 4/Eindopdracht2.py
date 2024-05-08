ee67dcb11eafe0be4024c5174de34eb38997ffa5
NickPolderHAN <N.vandepolder@student.han.nl>
2024-05-08 13:36:18 +0200

    changes to eindopdracht2

--- Eindopdract periode 4/Eindopdracht2.py
+++ Eindopdract periode 4/Eindopdracht2.py
@@ -1,5 +1,11 @@
 from medmnist import ChestMNIST
 
-dataset_train = ChestMNIST(split='train', download=True, size=128)
-dataset_test = ChestMNIST(split='test', download=True, size=128)
-print(dataset_train, dataset_test)
+def get_dataset():
+    dataset_train = ChestMNIST(split='train', download=True, size=128)
+    dataset_test = ChestMNIST(split='test', download=True, size=128)
+
+    return dataset_train, dataset_test
+
+
+if __name__ == '__main__':
+    get_dataset()
