ab0de6b57d1b590fcbe0f03be14f90f360bcc8f4
NickPolderHAN <N.vandepolder@student.han.nl>
2024-05-08 14:26:21 +0200

    changes to eindopdracht2

--- Eindopdract periode 4/Eindopdracht2.py
+++ Eindopdract periode 4/Eindopdracht2.py
@@ -1,10 +1,40 @@
 from medmnist import ChestMNIST
+import tensorflow
 
 
 def get_dataset():
+    # The pictures and labels for the train set.
+    pictures_train = []
+    labels_train = []
+
+    # The pictures and labels for the test set.
+    pictures_test = []
+    labels_test = []
+
     # Data ophalen en splitsen in train en test sets.
     dataset_train = ChestMNIST(split='train', download=True, size=128)
     dataset_test = ChestMNIST(split='test', download=True, size=128)
+
+    # Amount of labels.
+    label_count = 14
+
+    # The training set.
+    for i in dataset_train:
+        if 1 in i[1]:
+            pictures_train.append(i[0])
+            labels_train.append(1)
+        else:
+            pictures_train.append(i[0])
+            labels_train.append(0)
+
+    # The test set.
+    for i in dataset_test:
+        if 1 in i[1]:
+            pictures_test.append(i[0])
+            labels_test.append(1)
+        else:
+            pictures_test.append(i[0])
+            labels_test.append(0)
 
     return dataset_train, dataset_test
 
