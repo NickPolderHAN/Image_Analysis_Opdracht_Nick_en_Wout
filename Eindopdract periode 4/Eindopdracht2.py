3b36213a8afa0f2279924df070b95e3d916ce79f
NickPolderHAN <N.vandepolder@student.han.nl>
2024-05-08 13:42:24 +0200

    changes to eindopdracht2

--- Eindopdract periode 4/Eindopdracht2.py
+++ Eindopdract periode 4/Eindopdracht2.py
@@ -1,6 +1,8 @@
 from medmnist import ChestMNIST
 
+
 def get_dataset():
+    # Data ophalen en splitsen in train en test sets.
     dataset_train = ChestMNIST(split='train', download=True, size=128)
     dataset_test = ChestMNIST(split='test', download=True, size=128)
 
@@ -9,3 +11,12 @@ def get_dataset():
 
 if __name__ == '__main__':
     get_dataset()
+
+
+# De images worden correct voorbewerkt.
+# Model wordt bewaard en is beschikbaar voor toekomstig gebruik.
+# Het classificatie model is correct gegenereerd.
+# Splisting van data is efficient en correct
+# Classificatieperformance is acceptabel (>80% correct)
+# Validatie van het classificatiemodel is aantoonbaar correct
+# Preventie van overfitting is correct toegepast.
