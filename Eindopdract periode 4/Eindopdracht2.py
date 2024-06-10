be0566722835c687dea6b6598e6cbd43fabc747f
NickPolderHAN <N.vandepolder@student.han.nl>
2024-06-10 12:51:37 +0200

    Script up to date en documentatie toegevoegd.

--- Eindopdract periode 4/Eindopdracht2.py
+++ Eindopdract periode 4/Eindopdracht2.py
@@ -8,6 +8,7 @@ from tensorflow.keras import layers
 from PIL import Image
 import matplotlib.pyplot as plt
 from sklearn.metrics import roc_curve, auc
+import joblib
 
 
 def get_dataset():
@@ -27,7 +28,7 @@ def get_dataset():
     labels = []
     # Data ophalen en splitsen in train en test sets.
     dataset_train = ChestMNIST(split='train', download=True, size=128)
-
+    dataset_test = ChestMNIST(split='test', download=True, size=128)
     # Populate pictures and labels for training
     for image, label in dataset_train:
         pictures.append(np.array(image))
@@ -42,8 +43,7 @@ def get_dataset():
     return pictures_train, labels_train, pictures_valid, labels_valid, pictures_test, labels_test
 
 
-def merge_picture_and_label(pictures_train, labels_train, pictures_valid,
-                            labels_valid, pictures_test, labels_test):
+def merge_picture_and_label(pictures_train, labels_train, pictures_valid, labels_valid, pictures_test, labels_test):
     """
     Merges the labels and pictures of the training, testing and validation sets together
     into their own respective sets.
@@ -59,24 +59,19 @@ def merge_picture_and_label(pictures_train, labels_train, pictures_valid,
     - test_ds_ -> Contains the test dataset.
     - val_ds_ -> Contains the validation dataset.
     """
-    batch_size = 100
-
+    batch_size = 140
     # Convert to Tensorflow dataset.
     train_ds_ = tf.data.Dataset.from_tensor_slices(
         (np.array(pictures_train), np.array(labels_train)))  # Convert to numpy array
-
     # Convert to Tensorflow dataset.
     test_ds_ = tf.data.Dataset.from_tensor_slices(
         (np.array(pictures_test), np.array(labels_test)))  # Convert to numpy array
-
     # Convert to Tensorflow dataset.
     val_ds_ = tf.data.Dataset.from_tensor_slices((np.array(pictures_valid), np.array(labels_valid)))
-
     # Set batch size for each data sample.
     train_ds_ = train_ds_.batch(batch_size)
     test_ds_ = test_ds_.batch(batch_size)
     val_ds_ = val_ds_.batch(batch_size)
-
     return train_ds_, test_ds_, val_ds_
 
 
@@ -96,27 +91,25 @@ def model_training(train_data, test_data):
     # Store image sizes.
     img_height = 128
     img_width = 128
-
     # Creating data augmentation layers
-    data_augmentation = Sequential(
-        [
-            layers.RandomFlip("horizontal"),
-            layers.RandomRotation(0.1),  # Reduce rotation degree for medical images
-            layers.RandomZoom(0.2),  # Include random zoom
-            layers.RandomContrast(0.2),  # Adjust contrast
-            layers.RandomBrightness(0.2),  # Adjust brightness
-            layers.RandomTranslation(0.1, 0.1),  # Translate images slightly
-        ]
-    )
-
+    # data_augmentation = Sequential(
+    #     [
+    #         layers.RandomFlip("horizontal"),
+    #         layers.RandomRotation(0.1),  # Reduce rotation degree for medical images
+    #         layers.RandomZoom(0.2),  # Include random zoom
+    #         layers.RandomContrast(0.2),  # Adjust contrast
+    #         layers.RandomBrightness(0.2),  # Adjust brightness
+    #         layers.RandomTranslation(0.1, 0.1),  # Translate images slightly
+    #     ]
+    # )
     # Building the model
     model = Sequential([
-        data_augmentation,
+        # data_augmentation,
         layers.Rescaling(1. / 255),
         layers.BatchNormalization(axis=1, name='bn_conv1'),
         layers.Conv2D(16, 3, padding='same', activation='relu'),
-        layers.Conv2D(16, 3, padding='same', activation='relu'),
         layers.MaxPooling2D(),
+        layers.Conv2D(32, 3, padding='same', activation='relu'),
         layers.Conv2D(32, 3, padding='same', activation='relu'),
         layers.Conv2D(32, 3, padding='same', activation='relu'),
         layers.MaxPooling2D(),
@@ -126,7 +119,7 @@ def model_training(train_data, test_data):
         layers.Conv2D(124, 3, padding='same', activation='relu'),
         layers.Conv2D(124, 3, padding='same', activation='relu'),
         layers.MaxPooling2D(),
-        layers.Dropout(0.1),
+        layers.Dropout(0.7),
         layers.Flatten(),
         layers.Dense(124, activation='relu'),
         layers.Dense(1, activation="sigmoid")
@@ -140,41 +133,34 @@ def model_training(train_data, test_data):
     # Print model summary
     model.summary()
     # Fitting the model.
-    epochs = 5
+    epochs = 15
     history = model.fit(
         train_data,
         validation_data=test_data,
         epochs=epochs
     )
-
+    joblib.dump(model, "model_file")
     # Makes predictions using the test data.
     predictions = model.predict(test_data)
-
     # Assuming labels are binary (0 or 1)
     y_score = predictions.flatten()
     y_true = test_data.map(lambda x, y: y).unbatch()  # Extract true labels
     y_true = [label.numpy() for label in y_true]
-
     # plot_roc_curve(y_true, y_score)
-
     # Store the accuracy history.
     acc = history.history['accuracy']
     val_acc = history.history['val_accuracy']
-
     # Store the loss history.
     loss = history.history['loss']
     val_loss = history.history['val_loss']
-
     # Gets the epoch range used.
     epochs_range = range(epochs)
-
     plt.figure(figsize=(8, 8))
     plt.subplot(1, 2, 1)
     plt.plot(epochs_range, acc, label='Training Accuracy')
     plt.plot(epochs_range, val_acc, label='Validation Accuracy')
     plt.legend(loc='lower right')
     plt.title('Training and Validation Accuracy')
-
     plt.subplot(1, 2, 2)
     plt.plot(epochs_range, loss, label='Training Loss')
     plt.plot(epochs_range, val_loss, label='Validation Loss')
@@ -199,7 +185,6 @@ def plot_roc_curve(y_true, y_score):
     # With the y_true being the
     fpr, tpr, thresholds = roc_curve(y_true, y_score)
     roc_auc = auc(fpr, tpr)
-
     # Plot ROC curves
     plt.figure()
     plt.plot(fpr, tpr, color='darkorange', lw=2, label='ROC curve (area = %0.2f)' % roc_auc)
